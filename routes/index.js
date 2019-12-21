d98c25bc7e6e39b01b579337ed0cec3865b42365
David Chen <>
2019-12-21 00:58:54 -0500

    20191221

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('Passcode', { layout: 'Passcode_layout' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { layout: 'hero_layout' });
+  res.render('about');
 });
 
 module.exports = router;
