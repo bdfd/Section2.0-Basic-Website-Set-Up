e5b50a863e66650091b7a6c59eb79ffaa8f4e28e
David Chen <bdfd2005@gmail.com>
2019-12-22 23:41:59 -0500

    initial set up

--- routes/bdfd.js
+++ routes/bdfd.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('functions/1PhotoAnimation/4PhotoAlbum');
+  res.render('bdfd/index');
 });
 
 module.exports = router;
