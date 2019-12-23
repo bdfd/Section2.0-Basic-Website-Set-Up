f5df2168c3c666b496f6fd06416e015396c3ec35
David Chen <bdfd2005@gmail.com>
2019-12-23 01:37:20 -0500

    1

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var bdfdRouter = require('./routes/bdfd');
-var psintroRouter = require('./routes/personalintro');
+// var psintroRouter = require('./routes/personalintro.js');
 var psexpRouter = require('./routes/personalexperience');
-var pspjRouter = require('./routes/personalproject');
-var pseventRouter = require('./routes/personalevent');
+// var pspjRouter = require('./routes/personalproject');
+// var pseventRouter = require('./routes/personalevent');
 
 var app = express();
 
@@ -38,10 +38,10 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/bdfd', bdfdRouter);
-app.use('/personalintro', psintroRouter);
+// app.use('/personalintro', psintroRouter);
 app.use('/personalexperience', psexpRouter);
-app.use('/personalproject', pspjRouter);
-app.use('/personalevent', pseventRouter);
+// app.use('/personalproject', pspjRouter);
+// app.use('/personalevent', pseventRouter);
 
 
 // catch 404 and forward to error handler
---
+++ routes/personalexperience.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/01', function(req, res, next) {
+    res.render('/personal_experience/01');
+});
+
+router.get('/01', function(req, res, next) {
+    res.render('/personal_experience/02');
+});
+
+
+router.get('/01', function(req, res, next) {
+    res.render('/personal_experience/03');
+});
+
+router.get('/01', function(req, res, next) {
+    res.render('/personal_experience/04');
+});
+module.exports = router;
