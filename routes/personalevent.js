c5297897a211287bbeef1cb89d92c79313e931a0
David Chen <bdfd2005@gmail.com>
2019-12-24 15:58:02 -0500

    second photo effect

--- routes/personalevent.js
+++ routes/personalevent.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/2019', function(req, res, next) {
-    res.render('personal_event/2019', { layout: 'HP_Event2019'});
+    res.render('personal_event/2019/2019', { layout: 'HP_Event2019'});
 });
 
 router.get('/2019/1031', function(req, res, next) {
-    res.render('personal_event/20191031',{ layout: 'Empty_layout'});
+    res.render('personal_event/2019/20191031',{ layout: 'Empty_layout'});
+});
+
+router.get('/2019/1208', function(req, res, next) {
+    res.render('personal_event/2019/20191208',{ layout: 'Empty_layout'});
 });
 
 router.get('/2020', function(req, res, next) {
